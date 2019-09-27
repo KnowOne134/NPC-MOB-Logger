1ab17ad6adf82c7f5835dfc53df9a8b4715479d2
David Norris <Knowone134@gmail.com>
2019-09-27 18:28:55 -0500

    1st setup

---
+++ Bearclaw Pinnacle.lua
@@ -0,0 +1,5 @@
+local zone_database =
+{
+    [16801894] = {['id']=16801894, ['name']="WindPillar", ['polutils_name']="Wind Pillar", ['npc_type']="Simple NPC", ['index']=102, ['x']=119.997, ['y']=-171.415, ['z']=759.979, ['r']=0, ['flag']=32769, ['speed']=50, ['speedsub']=50, ['animation']=0, ['animationsub']=0, ['namevis']=0, ['status']=0, ['flags']=3, ['name_prefix']=0, ['look']="00003200", ['raw_packet']="0E2217006660000166000F0077FEEF423D6A2BC3A8FE3D440180000032326400000300000000000000000000000000000000320057696E6450696C6C6172000000000000", ['level']='?'},
+}
+return zone_database
